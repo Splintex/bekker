bb6bbc8de0ced16a1fb8ead60d82c6dd08f3f565
Splintex <godun.yevgen@gmail.com>
2013-11-26 20:18:09 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
         var cycle_next = $(this).parent().find(".js-next");
         var pager = $(this).parent().parent().find(".js-pager");
         $(this).cycle({
-            speed: 600,
+            speed: 200,
             fx: "carousel",
             timeout: 0,
             prev: cycle_prev,
@@ -27,5 +27,39 @@ $(document).ready(function() {
         });
 
     });
+
+    $(".js-search-example").click(function (){
+        var text = $(this).text();
+        $(this).parents(".header__search").find(".input").val(text);
+    })
+    
+
+    function init_cycle() {
+        if ($(".js-slider").length > 0) {
+
+            $(".js-slider").each(function(){
+                var slider_1 = $(this).find('.js-cycle-1');
+                var slider_2 = $(this).find('.js-cycle-2');
+                var prev_nav = $(this).find('.js-cycle-prev');
+                var next_nav = $(this).find('.js-cycle-next');
+                slider_1.cycle({
+                    prev: prev_nav,
+                    next: next_nav
+                });
+                slider_2.cycle();
+
+                var slideshows = $(this).find('.js-cycle').on('cycle-next cycle-prev', function(e, opts) {
+                    // advance the other slideshow
+                    slideshows.not(this).cycle('goto', opts.currSlide);
+                });
+
+                slider_2.find(".cycle-slide").click(function(){
+                    var index = slider_2.data('cycle.API').getSlideIndex(this);
+                    slideshows.cycle('goto', index);
+                });
+            });
+        }
+    }
+    init_cycle();
     
 }); 
