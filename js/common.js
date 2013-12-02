04cee6d1db4faf58a121bdd707109ef5cafc9240
Splintex <godun.yevgen@gmail.com>
2013-12-03 01:12:33 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     $(document).click(function() {
         $(".js-drop ul").hide();
+        $(".js-select-list").hide();
+        $(".js-select").removeClass("is-active");
     });
 
     $('.js-cycle').each(function(){
@@ -127,32 +129,32 @@ $(document).ready(function() {
             var select_list = $(this).parent().find(".js-select-list");
             var text = select_list.find("li").first().text();
             $(this).find(".js-select-text").text(text);
-            $(".js-select").removeClass("is-active");
-            $(".js-select-list").hide();
-            $(this).click(function(){
+            $(this).click(function(event){
                 if ($(this).hasClass("is-active")) {
                     $(this).removeClass("is-active");
                     select_list.slideUp("fast");
                 }
                 else {
+                    $(".js-select").removeClass("is-active");
+                    $(".js-select-list").hide();
                     select_list.slideDown("fast");
                     $(this).addClass("is-active");
                 }
+                event.stopPropagation();
             });
-            select_list.find("li").click(function() {
+            select_list.find("li").click(function(event) {
                 var id = $(this).attr("data-id");
                 var text = $(this).text();
                 $(this).parent().parent().find(".js-select-text").text(text);
                 $(this).parent().parent().find(".js-select-input").val(id);
                 $(this).parent().hide();
                 $(this).parents(".js-select").removeClass("is-active");
+                event.stopPropagation();
             });
         });
     }
     select();
-    $('.js-select-list').click(function(event){
-        event.stopPropagation();
-    }); 
+    
     $('.js-select').click(function(event){
         event.stopPropagation();
     });
